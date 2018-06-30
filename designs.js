9b322f317adf417b48f09f881c3fdb96325b082d
DAVID  DENNIS <39468399+daviddennis02@users.noreply.github.com>
2018-06-30 22:40:30 +0100

    Add files via upload
    All files are required for the web app to work properly

---
+++ designs.js
@@ -0,0 +1,51 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+let height, width, color, reset;
+function makeGrid() {
+
+// Your code goes here!
+ $("#pixel_canvas").html("");
+    height = $("#input_height").val();
+    width = $("#input_width").val();
+    if (height > 50 || width > 50 || height < 1 || width < 1) {
+        if (!error.classList.contains("error")) {
+            error.classList.toggle("error");
+            error.innerText = "the dimension has to be smaller than 50 and bigger than 0";
+            backUp();
+        }
+    } else {
+        error.innerText = "";
+        $("div").removeClass("error");
+        for (let x = 0; x < height; x++) {
+            $('#pixel_canvas').append('<tr></tr>');
+        }
+        for (let y = 0; y < width; y++) {
+            $('#pixel_canvas tr').each(function () {
+                $(this).append('<td></td>');
+            });
+        }
+    }
+}
+color = $('#colorPicker');
+$(document).on("mousedown", "tr td", function () {
+    let colorValue = color.val();
+    $(this).css('background-color', colorValue);
+    $('tr td').bind("mousemove", function () {
+        let colorValue = color.val();
+        $(this).css('background-color', colorValue);
+    }).mouseup(function() {
+        $('td').unbind('mousemove');
+    });
+    $('tr td').on('dblclick',function () {
+        $(this).css('background-color', "#79caf9")
+    })
+});
+reset = $("#pixel_canvas").html();
+function backUp() {
+    $("#pixel_canvas").html(reset);
+
+}
+
+
